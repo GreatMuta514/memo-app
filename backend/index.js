42e330b4ae9ab9524307853b67932b943eae00de
Daisuke Shibata <shiba12345678@icloud.com>
2023-10-06 15:47:31 +0900

    フロントエンドの構築と接続

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
   });
 
-app.get('/api', (req,res) => {
+app.get('/api', (req, res) => {
 	res.json({message: "hello world"});
 });
 
 
 app.listen(port, () => {
-console.log(`listening on *:${port}`);
+	console.log(`listening on *:${port}`);
 })
