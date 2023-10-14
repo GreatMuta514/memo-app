9b11ae0da5f9b95e7a8f826a6684e5162a2938af
Daisuke Shibata <shiba12345678@icloud.com>
2023-10-14 15:23:04 +0900

    db名の変更

--- backend/pool.js
+++ backend/pool.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 const pool = new Pool({
 	host: 'localhost',
-    database: 'memo_app',
+    database: 'memo_app_db',
     port: 5432,
     user: 'greatmuta',
     password: '',
